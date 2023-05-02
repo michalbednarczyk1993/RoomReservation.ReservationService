d82fc142ea761f8fde2ffc67f8e1e7d3dbc06d4c
michalbednarczyk1993 <michalbednarczyk1993@gmail.com>
2023-05-02 15:53:27 +0200

    API description (pt.2)

--- src/main/java/com/roomreservation/reservationservice/rest/api/PriceCountingService.java
+++ src/main/java/com/roomreservation/reservationservice/rest/api/PriceCountingService.java
@@ -21,18 +21,35 @@ public class PriceCountingService {
      *      }
      *  }
      *
+     * @param roomTypes - list of room types
+     * @param services - list of all possible combinations of available services
      */
     @GetMapping
     public void getActualPrices(List<String> roomTypes, List<String> services) {
-
+        // Oblicza ceny(roomTypes, services);
+        // w jakiś magiczny sposób muszę ustawić cenę bazową i opłaty za serwisy
+        // docelowo oczywiście powinny być od tego endpointy
     }
 
 
+    /**
+     * Return recalculated value in expected currency.
+     * Exchange course is taken from NBP API once a day by automated scheduler and stored with information about date.
+     * In case it will be null, then currency should be recalculated and returned base ond most recent values
+     * additionally an extra request will be sent to external API.
+     * ... internal logic described below:
+     * In case of no response or error message (from NBP API), request should be repeated 5 times.
+     * If fifth response from NBP will be failed, then HTTP 500 should be returned here with specified information.
+     *
+     * @param value - amount of money in actual currency
+     * @param actual - currency in which money are already presented
+     * @param expected - currency to which money should be recalculated
+     * @return amount of money in expected currency
+     */
     @GetMapping
     public Double recalculateToOtherCurrency(Double value, Currency actual, Currency expected) {
         return null;
     }
-
 
     private enum Currency {
         PLN,
