c750ca3640e473a138ca52cc1ac79522bff84a07
michalbednarczyk1993 <michalbednarczyk1993@gmail.com>
2023-10-27 14:39:40 +0200

    Status

--- src/main/java/com/roomreservation/reservationservice/core/repository/ReservationRepository.java
+++ src/main/java/com/roomreservation/reservationservice/core/repository/ReservationRepository.java
@@ -2,6 +2,7 @@ package com.roomreservation.reservationservice.core.repository;
 
 import com.roomreservation.reservationservice.core.entities.ReservationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Integer> {
 }
